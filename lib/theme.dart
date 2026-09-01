ab774fd2f98d59550d96183fa21fbe4324c879d4
snehaxdev <sneha.yadav.cc.2009@gmail.com>
2026-09-01 19:18:01 +0530

    Complete campus map search and theme

---
+++ lib/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CampusNavColors {
+  static const Color navy = Color(0xFF0B1F3A);
+  static const Color amber = Color(0xFFF59E0B);
+  static const Color teal = Color(0xFF06B6D4);
+  static const Color background = Color(0xFFF5F8FA);
+  static const Color white = Colors.white;
+}
